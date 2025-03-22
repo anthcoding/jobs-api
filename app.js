c3441aeeefa11d6ce6e89a1c9d9e2b988e1c2483
Anthony Lorenzo <anth.coding@gmail.com>
2025-03-22 16:09:37 -0400

    Controllers, routes and error handlers

--- app.js
+++ app.js
@@ -2,18 +2,19 @@ require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
 const app = express();
+const connectDB = require('./db/connect');
+const authMiddleware = require('./middleware/authentication');
 
-// error handler
+const authRoutes = require('./routes/auth');
+const jobsRoutes = require('./routes/jobs');
+
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(express.json());
-// extra packages
 
-// routes
-app.get('/', (req, res) => {
-  res.send('jobs api');
-});
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/jobs', authMiddleware, jobsRoutes);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -22,9 +23,8 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => console.log(`Server is listening on port ${port}...`));
   } catch (error) {
     console.log(error);
   }
