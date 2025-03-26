d071195a2ad54e2224a840034ef780e9facfc4da
Anthony Lorenzo <anth.coding@gmail.com>
2025-03-26 14:00:34 -0400

    Security changes

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 require('dotenv').config();
 require('express-async-errors');
+
+//extra security packages
+const helmet = require('helmet');
+//cors allows to make request from different origins
+const cors = require('cors');
+//cross site scripting
+const xss = require('xss-clean');
+//rate limiting
+const rateLimiter = require('express-rate-limit');
+
 const express = require('express');
 const app = express();
 const connectDB = require('./db/connect');
@@ -11,7 +21,17 @@ const jobsRoutes = require('./routes/jobs');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.use(
+  rateLimiter.limiter({
+    windowMs: 15 * 60 * 1000,
+    limit: 100,
+    message: 'Too many requests from this IP, please try again later',
+  })
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/jobs', authMiddleware, jobsRoutes);
